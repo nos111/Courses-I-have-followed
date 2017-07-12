e62b23643c145c2d4aec0fe59d229c8475456d2c
nos111 <nsaffour@gmail.com>
2017-07-12 22:13:33 +0200

    completed assignment 9

--- Introduction to Computer Science (fall 2008)/assignment9/ps9.py
+++ Introduction to Computer Science (fall 2008)/assignment9/ps9.py
@@ -9,7 +9,7 @@ from string import *
 class Shape(object):
     def area(self):
         raise AttributeException("Subclasses should override this method.")
-
+    
 class Square(Shape):
     def __init__(self, h):
         """
@@ -52,6 +52,22 @@ class Circle(Shape):
 
 #
 # Problem 1: Create the Triangle class
+class Triangle(Shape):
+    def __init__(self, b, h):
+        self.height = float(h)
+        self.base = float(b)
+    def area(self):
+        return self.base * self.height
+    def __str__(self):
+        return 'Triangle with  base' + str(self.base) + ' and height' + str(self.height)
+    def __eq__(self, other):
+        """
+        Two triangles are equal if they have the same height and base.
+        other: object to check for equality
+        """
+        return type(other) == Triangle and self.height == other.height and self.base == other.base
+
+
 #
 ## TO DO: Implement the `Triangle` class, which also extends `Shape`.
 
@@ -66,27 +82,40 @@ class ShapeSet:
         """
         Initialize any needed variables
         """
-        ## TO DO
+
+        self.shape = []
+        
     def addShape(self, sh):
-        """
+                """
         Add shape sh to the set; no two shapes in the set may be
         identical
         sh: shape to be added
         """
-        ## TO DO
+                if sh not in self.shape:
+                    self.shape.append(sh)
+        
+ 
     def __iter__(self):
         """
         Return an iterator that allows you to iterate over the set of
         shapes, one shape at a time
         """
-        ## TO DO
+        for x in self.shape:
+            yield x
+      
     def __str__(self):
         """
         Return the string representation for a set, which consists of
         the string representation of each shape, categorized by type
         (circles, then squares, then triangles)
         """
-        ## TO DO
+        string = ""
+        for x in self.shape:
+            string = string + str(x) + "\n"
+        return string
+
+
+
         
 #
 # Problem 3: Find the largest shapes in a ShapeSet
@@ -98,6 +127,30 @@ def findLargest(shapes):
     shapes: ShapeSet
     """
     ## TO DO
+    #find the object with the biggest area
+    #note this will return only one object that's why we need to check
+    #if there is other objects with the same area so we can add them to the tuble
+    maxArea = max(shapes, key=lambda shape: shape.area())
+    shapesAreas = []
+    shapesAreas.append(maxArea)
+    
+    for shape in shapes:
+        if shape.area() == maxArea.area():
+            if shape != maxArea:
+                shapesAreas.append(shape)
+    return shapesAreas
+    
+ss = ShapeSet()
+t = Triangle(4,6)
+ss.addShape(Circle(1))
+ss.addShape(t)
+ss.addShape(Triangle(3,8))
+largest = findLargest(ss)
+for e in largest:
+    print e
+
+
+    
 
 #
 # Problem 4: Read shapes from a file into a ShapeSet
@@ -108,4 +161,28 @@ def readShapesFromFile(filename):
     Creates and returns a ShapeSet with the shapes found.
     filename: string
     """
-    ## TO DO
+    #initiate shape set
+    shapeSet = ShapeSet()
+    #open the file
+    inputFile = open(filename)
+    #loop over the file
+    for line in inputFile:
+        #remove the white space from eachline
+        line = line.strip()
+        #split the line to a list of items
+        line = line.split(',')
+        if line[0].lower() == 'circle':
+            shape = Circle(line[1])
+        if line[0].lower() == 'triangle':
+            shape = Triangle(line[1],line[2])
+        if line[0].lower() == 'square':
+            shape = Square(line[1])
+        shapeSet.addShape(shape)
+        
+    return shapeSet
+    
+
+ss = readShapesFromFile('shapes.txt')
+print ss
+
+    
